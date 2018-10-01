b2b4e59d6036ec2333c11f3e44bf62e504607f31
Daniel Hendricks <mail@danhendricks.com>
2018-10-01 00:51:55 -0500

    Merged senlin@8c3b368: Internationalize help tab strings

--- template/help.php
+++ template/help.php
@@ -21,8 +21,10 @@ $videos = array(
 
 <div class="help-tab content-tab">
 	<div class="support">
-		<h3>Support</h3>
-		<p>Please report bugs or ask questions in the <a href="https://github.com/wp-sync-db/wp-sync-db/issues">GitHub Issue Tracker</a>.</p>
+		<h3><?php _e( 'Support', 'wp-sync-db' ); ?></h3>
+		<p>
+			<?php printf( __( 'Please report bugs or ask questions in the <a href="%s">GitHub Issue Tracker</a>.', 'wp-sync-db' ), 'https://github.com/dmhendricks/wp-sync-db/issues' ); ?>
+		</p>
 	</div>
 	<div class="debug">
 		<h3><?php _e( 'Diagnostic Info &amp; Error Log', 'wp-sync-db' ); ?></h3>
